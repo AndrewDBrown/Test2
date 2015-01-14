dd6ce3afa95795610e93fddf1f3f7254a9cd47a9
Andy Brown <andrewbrown@callibre.com>
2015-01-14 10:12:06 +0000

    asdfasfaf

---
+++ Set Drives.bat
@@ -0,0 +1,10 @@
+Subst G: /D
+Subst M: /D
+Subst N: /D
+Subst O: /D
+Subst T: /D
+Subst W: /D
+Subst Y: /D
+Subst Z: /D
+Subst O: "D:\Database\TeleStudio Clients\Callibre"
+Subst Z: "D:\Database\TeleStudio"
